2dd5973c55382441ff77d1cfe99df7983cba2f03
ReiLennux <lennyrm343@outlook.com>
2025-11-13 16:13:45 -0600

    Restaurant Cards

--- src/features/lenn/menu/restaurant/RestaurantAddressCard.jsx
+++ src/features/lenn/menu/restaurant/RestaurantAddressCard.jsx
@@ -2,15 +2,22 @@ export function RestaurantAddressCard({ address, ...props }) {
   const { street, city, state, zip, country } = address;
   return (
     <div {...props}>
-      <h1 className="text-lg">
-        <p>
-          <span>📌</span> {street}, {zip}
+      <h1 className="leading-relaxed">
+        <p className="mb-2 flex items-center gap-2">
+          <span className="text-xl">📌</span>
+          <span className="text-stone-800 font-medium">
+            {street}, {zip}
+          </span>
         </p>
-        <p>
-          <span>🌆</span> {city}, {state}
+        <p className="mb-2 flex items-center gap-2">
+          <span className="text-xl">🌆</span>
+          <span className="text-stone-800 font-medium">
+            {city}, {state}
+          </span>
         </p>
-        <p>
-          <span>🌍</span> {country}
+        <p className="flex items-center gap-2">
+          <span className="text-xl">🌍</span>
+          <span className="text-stone-800 font-medium">{country}</span>
         </p>
       </h1>
     </div>
--- src/features/lenn/menu/restaurant/RestaurantContactCard.jsx
+++ src/features/lenn/menu/restaurant/RestaurantContactCard.jsx
@@ -5,30 +5,41 @@ export function RestaurantContactCard({ contact, ...props }) {
   for (let key in social) {
     if (Object.hasOwn(social, key)) {
       elements.push(
-        <a className="w-fit" key={key} href={social[key]}>
+        <a
+          className="w-fit hover:opacity-70 transition-opacity"
+          key={key}
+          href={social[key]}
+        >
           <img
             alt={`${key} social link`}
             src={`https://www.google.com/s2/favicons?domain=${key}.com&sz=128`}
-            className="w-10 h-10"
+            className="w-10 h-10 rounded-lg"
           />
         </a>
       );
     }
   }
-
   return (
     <div {...props}>
-      <p className="break-words">
-        <span>☎️</span> <a href={`tel:${phone}`}>{phone}</a>
+      <p className="mb-3 flex items-center gap-2">
+        <span className="text-xl">☎️</span>
+        <a
+          href={`tel:${phone}`}
+          className="text-stone-800 font-medium hover:text-stone-900 transition-colors"
+        >
+          {phone}
+        </a>
       </p>
-      <p className="break-words">
-        <span>📧</span> <a href={`mailto:${email}`}>{email}</a>
+      <p className=" mb-4 flex items-center gap-2">
+        <span className="text-xl">📧</span>
+        <a
+          href={`mailto:${email}`}
+          className="text-stone-800 font-medium hover:text-stone-900 transition-colors"
+        >
+          {email}
+        </a>
       </p>
-      <div
-        className={`py-4 grid grid-cols-${elements.length} place-items-center`}
-      >
-        {elements}
-      </div>
+      <div className={`py-2 flex gap-4 justify-center`}>{elements}</div>
     </div>
   );
 }
