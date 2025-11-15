47a4ecaf3f8076f2c8c0fd5820626b285d2e168d
Orlando G.V <orlando.gal.var05@gmail.com>
2025-11-15 08:23:21 -0600

    fix: correcciones menores en estilos

--- src/features/menu-billy-blanco-orlando/components/MenuSurvey.jsx
+++ src/features/menu-billy-blanco-orlando/components/MenuSurvey.jsx
@@ -31,7 +31,7 @@ export default function MenuSurvey({
                 onClick={handlerCloseSurvey}
                 className="text-gray-500 hover:text-gray-700 text-2xl font-bold"
               >
-                ×
+                X
               </button>
             </div>
 
