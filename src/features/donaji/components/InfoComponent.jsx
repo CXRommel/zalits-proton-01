658c9342c5885f50cee71dcc2d375582e293da7e
donajivt <vaniadonajitorres@gmail.com>
2025-11-15 06:10:38 -0600

    Se agrego el InfoComponent

--- src/features/donaji/components/InfoComponent.jsx
+++ src/features/donaji/components/InfoComponent.jsx
@@ -76,6 +76,40 @@ function InfoRestaurantComponent({ menuData, lang }) {
         </div>
       </div>
 
+      <div className="bg-white rounded-xl p-4 flex items-start gap-3 shadow-md">
+        <img
+          src="https://media.istockphoto.com/id/1145172366/es/vector/icono-de-tel%C3%A9fono-vector-persona-usuario-hombre-perfil-avatar-s%C3%ADmbolo-para-contacto.jpg?s=612x612&w=0&k=20&c=gijOjCq3fBdLoBmO_SXyIlBK_8nll0kJ5u701g_4Zsc="
+          alt="Contacto"
+          className="w-7 h-7 opacity-80"
+        />
+        <div>
+          <h3 className="text-orange-950 font-semibold text-lg">
+            {lang === "en" ? "Contact" : "Contacto"}
+          </h3>
+          <p className="text-orange-950 text-sm leading-tight">
+            -{menuData.contact.phone}
+            <br />
+            {menuData.contact.email}
+            <br />
+            <a
+              href={menuData.contact.social.facebook}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              -Instagram
+            </a>
+            <a
+              href={menuData.contact.social.facebook}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="ml-7"
+            >
+              -Facebook
+            </a>
+          </p>
+        </div>
+      </div>
+
       {isSurveyEnabled && (
         <div>
           <button
